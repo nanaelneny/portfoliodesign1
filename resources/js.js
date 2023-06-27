afb9fdd0be7f72a03df4ce0b8fc143cf1cb0df0c
fokuoisaac <nanaelneny@gmail.com>
2023-06-27 15:02:23 +0000

    adding files to my github

---
+++ resources/js.js
@@ -0,0 +1,15 @@
+
+function flipImage(){
+    document.getElementById('flip-image')
+    .src="./resources/images/Image_Editor.png";
+    document.getElementById('changename')
+    .innerHTML="Witch-King of Angmar";
+};
+
+function flipBack(){
+    document.getElementById('flip-image')
+    .src="./resources/images/Cropped_Image.png";
+    document.getElementById('changename')
+    .innerHTML="Fokuo Isaac Opoku";
+};
+
